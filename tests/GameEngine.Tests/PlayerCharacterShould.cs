4b0d3b380042b4e3f22d2c7ccfd8c5e567bf146c
Andy Jones <26009223+jonesandy@users.noreply.github.com>
2021-01-11 21:12:40 +0000

    practising with string assertions.

--- tests/GameEngine.Tests/PlayerCharacterShould.cs
+++ tests/GameEngine.Tests/PlayerCharacterShould.cs
@@ -8,6 +8,51 @@ namespace GameEngine.Tests
         [Fact]
         public void BeInexperiencedWhenNew()
         {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.True(sut.IsNoob);
+        }
+
+        [Fact]
+        public void CalculateFullName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Andy";
+            sut.LastName = "Jones";
+
+            Assert.Equal("Andy Jones", sut.FullName);
+        }
+        
+        [Fact]
+        public void HaveFullNameStartingWithFirstName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Andy";
+
+            Assert.StartsWith("Andy", sut.FullName);
+        }
+
+        [Fact]
+        public void HaveFullNameEndingWithLastName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.LastName = "Jones";
+
+            Assert.EndsWith("Jones", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullNameIgnoringCase()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "JOHN";
+            sut.LastName = "SMITH";
+
+            Assert.Equal("John Smith", sut.FullName, ignoreCase: true);
         }
     }
 }
