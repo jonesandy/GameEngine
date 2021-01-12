d396310e259d4de362fa498585edd76e9df2e50c
Andy Jones <26009223+jonesandy@users.noreply.github.com>
2021-01-12 22:10:27 +0000

    Completed Assertions module.

---
+++ tests/GameEngine.Tests/EnemyFactoryShould.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class EnemyFactoryShould
+    {
+        [Fact]
+        public void CreateNormalEnemyByDefault()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie");
+
+            Assert.IsType<NormalEnemy>(enemy);
+        }
+
+        [Fact]
+        public void CreateBossEnemy()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            Assert.IsType<BossEnemy>(enemy);
+        }
+
+        [Fact]
+        public void CreateBossEnemy_AndAssignName()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            BossEnemy result = Assert.IsType<BossEnemy>(enemy);
+
+            Assert.Equal("Zombie King", result.Name);
+        }
+
+        [Fact]
+        public void CreateBossEnemy_AssertFromAssignableTypes()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            Assert.IsAssignableFrom<Enemy>(enemy);
+        }
+
+        [Fact]
+        public void CreateSeperateInstances()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy1 = sut.Create("Zombie");
+            Enemy enemy2 = sut.Create("Zombie");
+
+            Assert.NotSame(enemy1, enemy2);
+        }
+
+        [Fact]
+        public void ThrowExceptionWithNullName()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Assert.Throws<ArgumentNullException>(() => sut.Create(null));
+            Assert.Throws<ArgumentNullException>("name", () => sut.Create(null));
+        }
+
+        [Fact]
+        public void OnlyAllowsKingOrQueenBossEnemies()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            EnemyCreationException ex = 
+                Assert.Throws<EnemyCreationException>(() => sut.Create("Zombie", true));
+
+            Assert.Equal("Zombie", ex.RequestedEnemyName);
+            Assert.Contains("must end with", ex.Message);
+        }
+    }
+}
